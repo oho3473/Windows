5b744d2d28e69aca214c115e8da2fa8c6f624866
usw3473@gmail.com <usw3473@gmail.com>
2022-09-04 15:48:56 +0900

    초기 커밋.

---
+++ WinMain.cpp
@@ -0,0 +1,83 @@
+#include<Windows.h>
+
+const wchar_t gClassName[] = L"MyClass";
+
+LRESULT CALLBACK WindowProc(
+	HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam
+);
+
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd)
+{
+	HWND hWnd;
+	WNDCLASSEX wc;
+
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpszClassName = gClassName;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpfnWndProc = WindowProc;
+	wc.cbSize = sizeof(WNDCLASSEX);
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, L"Failed to Register WindowClass", L"Error",MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	hWnd = CreateWindowEx(NULL,
+		gClassName,
+		L"Hello Window",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		640,
+		480,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	if (hWnd == nullptr)
+	{
+		MessageBox(NULL, L"Failed to create window class", L"Error",
+			MB_ICONEXCLAMATION | MB_OK
+		);
+		return 0;
+	}
+
+	ShowWindow(hWnd, nShowCmd);
+	UpdateWindow(hWnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return static_cast<int>(msg.wParam);
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		case WM_CLOSE:
+			DestroyWindow(hWnd);
+			break;
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
